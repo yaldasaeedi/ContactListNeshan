4909836ff4a15d79f01c6178b862b5abf4fc81fa
yaldasaeedi <yalda.saeedi1381@gmail.com>
2023-08-29 11:43:51 +0330

    WIP : added view controllers

--- Contact List/ViewController.swift
+++ Contact List/ViewController.swift
@@ -3,15 +3,19 @@
 //  Contact List
 //
 //  Created by Helen Besharatian on 6/6/1402 AP.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    
+    @IBOutlet weak var searchForContactSB: UISearchBar!
+    @IBOutlet weak var contactTableTV: UITableView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
---
+++ Contact List/ViewControllerForAddUser.swift
@@ -0,0 +1,49 @@
+//
+//  ViewControllerForAddUser.swift
+//  Contact List
+//
+//  Created by Helen Besharatian on 6/7/1402 AP.
+//
+
+import UIKit
+
+class ViewControllerForAddUser: UIViewController {
+
+    @IBOutlet weak var userImageIV: UIImageView!
+    @IBOutlet weak var contactNameTF: UITextField!
+    @IBOutlet weak var contactNumderTF: UITextField!
+    @IBOutlet weak var contactEmailTF: UITextField!
+    @IBOutlet weak var contactBirthdayDP: UIDatePicker!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addingContactName(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func addingContactNumber(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func addingContactEmail(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func addingContactBirthdat(_ sender: Any){
+        
+        
+    }
+    
+    @IBAction func contactInfoAdded(_ sender: Any) {
+        
+        
+    }
+    
+    
+}
